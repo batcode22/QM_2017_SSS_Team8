84f206c72bc3c7a4d41c8e32c2df7b2009f1b665
alenaizan <alenaizana@gmail.com>
2017-08-01 15:32:30 -0400

    parallel jk using openmp

--- cpp_projects/build/JK.py
+++ cpp_projects/build/JK.py
@@ -2,7 +2,8 @@ import numpy as np
 import basic_mod as bm
 import psi4
 import time
-import jk
+import jkcomp
+import jkcomp_parallel
 
 print(bm.factorial(5))
 
@@ -17,7 +18,7 @@ H 1 1.1 2 104
 """)
 
 # Build a ERI tensor
-basis = psi4.core.BasisSet.build(mol, target="cc-pvdz")
+basis = psi4.core.BasisSet.build(mol, target="aug-cc-pvtz")
 mints = psi4.core.MintsHelper(basis)
 I = np.array(mints.ao_eri())
 
@@ -32,8 +33,12 @@ K_ref = np.einsum("prqs,rs->pq", I, D)
 
 # Your implementation
 start_time = time.time()
-J,K = jk.form_JK(I, D)
-print("--- %s seconds ---" % (time.time() - start_time))
+J,K = jkcomp_parallel.form_JK(I, D)
+print("parallel: --- %s seconds ---" % (time.time() - start_time))
+
+start_time = time.time()
+J,K = jkcomp.form_JK(I, D)
+print("serial: --- %s seconds ---" % (time.time() - start_time))
 
 # Make sure your implementation is correct
 print("J is correct: %s" % np.allclose(J, J_ref))
